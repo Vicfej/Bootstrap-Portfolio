8d60fdaff6f8f12db7264e4e5b46402c139b04c6
Vicfej <53326194+Vicfej@users.noreply.github.com>
2020-01-17 20:44:44 -0500

    New

---
+++ index.js
@@ -0,0 +1,11 @@
+// $('#myModal').modal(show)
+// var modal = document .getElementsByClassName("modal fade");
+// var btn = document.getElementById("contact");
+// var span = document.getElementsByClassName("close")[0];
+// btn.onclick = function(){
+//     modal.style.display = "block";
+// }
+$('#contact').on('click', function(){
+$('.contact-me').show();
+$(this).hide();
+});
